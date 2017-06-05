7ebf17da4cc4b3fca6e7389208449311f61d987b
daniel bellini <danny15002@gmail.com>
2017-06-05 01:25:05 -0500

    basic messaging features working

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const https = require('https');
+// const https = require('https');
 const http = require('http');
 const ws = require('ws');
 const fs = require('fs');
@@ -17,11 +17,46 @@ const socketServer = new ws.Server({
   server: server
 });
 
+const users = {};
 socketServer.on('connection', socket => {
-  socket.send('CONNECTED TO SERVER');
+  socket.send('{"message":"CONNECTED TO SERVER"}');
+
   socket.on('message', message => {
-    console.log('message');
-    socket.send('Echoing message: ' + message);
+    console.log(message);
+    let payload;
+    try {
+      payload = JSON.parse(message);
+    } catch (e) {
+      socket.send('{"error": "failed to parse payload"}');
+      return;
+    }
+
+    console.log('BOUT TO SWITCH DA BITCH');
+    switch (payload.action) {
+      case 'REGISTER':
+        console.log('registering');
+        if (users[payload.username] === undefined) {
+          users[payload.username] = socket;
+          console.log('USERS: ', Object.keys(users));
+          const responsePayload = {
+            action: 'REGISTER',
+            message: 'registered successfully',
+            username: payload.username,
+            users: Object.keys(users)
+          }
+          socket.send(JSON.stringify(responsePayload));
+        }
+      break;
+      case 'MESSAGE':
+        console.log('messaging');
+        const responsePayload = {
+          action: 'MESSAGE',
+          userFrom: payload.userFrom,
+          message: payload.message
+        };
+        users[payload.userTo].send(JSON.stringify(responsePayload));
+      break;
+    }
   });
 });
 
