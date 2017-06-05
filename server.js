63bce36e1aec3dc87950d8dd6e32194c93c47339
daniel bellini <danny15002@gmail.com>
2017-06-04 20:07:03 -0500

    added socket connection to server

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const https = require('https');
 const http = require('http');
+const ws = require('ws');
+const fs = require('fs');
+const indexCache = fs.readFileSync('./app/index.html', 'utf-8');
 
-const server = http.createServer((req, res) => {
-  res.write('Attempting to reach: ' + req.url);
-  res.end();
+const server = http.createServer((request, response) => {
+  console.log('serving request');
+  response.writeHead(200, {
+    'Content-Length': indexCache.length,
+    'Content-Type': 'text/html' });
+  response.write(indexCache);
+  response.end();
 });
 
+const socketServer = new ws.Server({
+  server: server
+});
+
+socketServer.on('connection', socket => {
+  socket.send('CONNECTED TO SERVER');
+  socket.on('message', message => {
+    console.log('message');
+    socket.send('Echoing message: ' + message);
+  });
+});
 
 const port = process.env.PORT || 8080;
-server.listen(port);
+server.listen(port, () => console.log('listening on port: ' + port));
 
 module.exports = server;
