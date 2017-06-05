f5dca02070cb66fe915a88f8ab103e2b0aca708b
daniel bellini <danny15002@gmail.com>
2017-06-04 19:07:57 -0500

    initial app setup

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const https = require('https');
 const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.write('Attempting to reach' + req.url());
+  res.end();
+});
+
+server.listen(9000);
+
+modules.export = server;
