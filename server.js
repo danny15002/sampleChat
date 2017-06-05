50a24746d5c771ea86d01246664770c164d07a70
daniel bellini <danny15002@gmail.com>
2017-06-04 19:10:06 -0500

    fixed some thigns

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const https = require('https');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write('Attempting to reach' + req.url());
+  res.write('Attempting to reach: ' + req.url);
   res.end();
 });
 
 server.listen(9000);
 
-modules.export = server;
+module.exports = server;
